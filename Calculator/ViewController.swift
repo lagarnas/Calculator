4cc6269cd06aa8eb419d11f1fd4f55cff49833f7
Leonteva Anastasia <lagarnas@gmail.com>
2020-02-24 22:14:21 +0400

    my first version

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,82 @@
 
 import UIKit
 
+
+enum MathOperation: Int {
+    case divide = 11, multy, minus, plus
+}
+
 class ViewController: UIViewController {
 
+    //число на экране
+    var numberOnScreen: Double = 0
+    //предыдущие числа
+    var previousNumber: Double = 0
+    //
+    var performingMath = true
+    //операция
+    var operation: MathOperation? = nil
+    
+    @IBOutlet weak var label: UILabel!
+    
+    //функция для чисел
+    @IBAction func numbers(_ sender: UIButton) {
+         if performingMath == true {
+            label.text = String(sender.tag)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+         }else {
+            label.text = label.text! + String(sender.tag)
+            numberOnScreen = Double(label.text!)!
+        }
+    }
+    
+    //функция для операций с числами
+    @IBAction func buttons(_ sender: UIButton) {
+        previousNumber = Double(label.text!)!
+        operation = MathOperation.init(rawValue: sender.tag)
+        performingMath = true
+        switch operation {
+            case .divide: //divide
+                label.text = "/"
+            case .multy: //multiply
+                label.text = "x"
+            case .minus: //minus
+                label.text = "-"
+            case .plus: //plus
+                label.text = "+"
+            default:
+                break
+        }
+    }
+    
+    
+    
+    @IBAction func calc(_ sender: UIButton) {
+        switch operation {
+        case .divide: //divide
+                label.text = String(previousNumber / numberOnScreen)
+        case .multy: //multiply
+                label.text = String(previousNumber * numberOnScreen)
+        case .minus: //minus
+                label.text = String(previousNumber - numberOnScreen)
+        case .plus: //plus
+                label.text = String(previousNumber + numberOnScreen)
+            default:
+                break
+        }
+    }
+    
+    //очищение
+    @IBAction func clear(_ sender: UIButton) {
+        label.text = ""
+        previousNumber = 0
+        numberOnScreen = 0
+        operation = nil
+        performingMath = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,4 +91,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
---
+++ Calculator/mathOperation.swift
@@ -0,0 +1,30 @@
+//
+//  MathOperation.swift
+//  Calculator
+//
+//  Created by Анастасия Лагарникова on 24.02.2020.
+//  Copyright © 2020 lagarnas. All rights reserved.
+//
+
+import UIKit
+
+class MathOperation: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
